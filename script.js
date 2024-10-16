0eeb9ea1fb59fb3c77a3c513072dacc9dfb74594
Guilherme Santos Quaresma <gsqguilhermesantos@gmail.com>
2024-10-16 16:42:31 -0300

    criação da lista de pedidos

---
+++ script.js
@@ -0,0 +1,37 @@
+let listaDePedidos = [];
+
+function adicionarPedido(id, nomeCliente, produto, quantidade) {
+    const pedido = {
+        id: id,
+        nomeCliente: nomeCliente,
+        produto: produto,
+        quantidade: quantidade
+    };
+    listaDePedidos.push(pedido);
+    console.log(`Pedido de ${produto} para ${nomeCliente} adicionado!`);
+}
+
+function listarPedidos() {
+    if (listaDePedidos.length === 0) {
+        console.log("Nenhum pedido na lista.");
+    } else {
+        listaDePedidos.forEach((pedido) => {
+            console.log(`ID: ${pedido.id} | Cliente: ${pedido.nomeCliente} | Produto: ${pedido.produto} | Quantidade: ${pedido.quantidade}`);
+        });
+    }
+}
+
+function removerPedido(id) {
+    listaDePedidos = listaDePedidos.filter(pedido => pedido.id !== id);
+    console.log(`Pedido ${id} removido.`);
+}
+
+adicionarPedido(1, "Esmael Abreu", "Bolo de Chocolate", 2);
+adicionarPedido(2, "Marina Almeida", "Torta de Limão", 1);
+adicionarPedido(3, "Cleissiane Fonseca", "Cupcake", 4);
+
+listarPedidos();
+
+removerPedido(2);
+
+listarPedidos();
